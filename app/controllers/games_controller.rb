cd3cab241aaebc0b0d94b9d8f595cf8633d9ee65
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-10-29 16:33:29 +0100

    done i guess

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,22 +2,26 @@ require 'json'
 require 'open-uri'
 
 class GamesController < ApplicationController
+
   def new
     @grid = []
-    8.times do
+    10.times do
       @grid << ('a'..'z').to_a.sample
     end
   end
 
   def score
-    return @score = "Sorry but #{params[:word]} cannot be built from #{params[:grid].split}" unless check_grid(params[:grid], params[:word])
+    return @score = "Sorry but #{params[:word]} cannot be built from #{params[:grid].split}, 0 point, noob" unless check_grid(params[:grid], params[:word])
 
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     word = JSON.parse(open(url).read)
+    points = params[:word].split("").size * 10
     if word["found"]
-      return @score = "Congratulation, #{params[:word].upcase} is an english word"
+      session[:answer] += points
+      return @score = "Congratulation, #{params[:word].upcase} is an english word, you won #{points} points"
     else
-      return @score = "Sorry but #{params[:word]} does not seem to be an english word"
+      session[:answer] -= points / 2
+      return @score = "Sorry but #{params[:word]} does not seem to be an english word, - #{points} point, noob"
     end
   end
 
